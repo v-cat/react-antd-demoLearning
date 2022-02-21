62bed38a5db6041386b9e7eda9527e067a4801a4
杨冬林 <18552281185@gts.work>
2022-02-22 00:12:46 +0800

    始化

---
+++ src/app/index.js
@@ -0,0 +1,4 @@
+const App = () => {
+  return "welcome";
+};
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 // (function () {
 //     console.log("hey mister")
 // }());
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createBrowserHistory } from 'history'
-import { Router, Route, Switch, Rsdirect } from 'react-router-dom'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+// import { Router, Route, Switch, Rsdirect } from "react-router-dom";
+import APP from "./app";
 // core components
-import Admin from 'layouts/Admin.jsx'
+import Admin from "layouts/Admin.jsx";
 
-import 'assets/css/material-dashboard-react.css?v=1.6.0'
+// import "assets/css/material-dashboard-react.css?v=1.6.0";
 
 // import 'index.scss'
 // let HelloWorld = () => {
 //   return <h1>Hello there World!</h1>
 // }
 
-const hist = createBrowserHistory()
+const hist = createBrowserHistory();
 ReactDOM.render(
+  <APP />,
   // <HelloWorld />,
-  <Router history={hist}>
-    <Switch>
-      <Route path="/admin" component={Admin} />
-      <Route path="/" to="/admin/dashboard" />
-    </Switch>
-  </Router>,
-  document.getElementById('root')
-)
+  // <Router history={hist}>
+  //   <Switch>
+  //     <Route path="/admin" component={Admin} />
+  //     <Route path="/" to="/admin/dashboard" />
+  //   </Switch>
+  // </Router>,
+  document.getElementById("root")
+);
